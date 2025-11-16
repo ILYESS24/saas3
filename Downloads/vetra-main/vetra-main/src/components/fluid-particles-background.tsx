6de6a950080302740f59975fec28ea8bbe6771b6
ILYESS24 <ilyessbenina3@gmail.com>
2025-11-16 01:51:45 +0100

    Fix import path in fluid-particles-background

--- Downloads/vetra-main/vetra-main/src/components/fluid-particles-background.tsx
+++ Downloads/vetra-main/vetra-main/src/components/fluid-particles-background.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useRef } from "react";
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib";
 
 interface CyberBackgroundProps {
   children?: React.ReactNode;
